6d4e654afe3379f2ab7d78f52ffc721fa978ef96
ct <catoro17@misena.edu.co>
2022-06-14 01:28:07 -0500

    version 3

--- rpsGame2.js
+++ rpsGame2.js
@@ -2,37 +2,48 @@ let optUser = parseInt(localStorage.keepNum);
 const optComp = Math.floor( Math.random() * ( 4 - 1 ) ) + 1; 
 const contenedorUser = document.querySelector(".containUser");
 const contenedorComp = document.querySelector(".containerComp"); 
-let img = "";
-let img2 = "";
+let userBool = false;
+let compBool = false;
 
-function res(option, image, contain){
-switch(option){
+function res(option,  contain, bool){
+    switch(option){
     case 1 :
+        if(bool)
         image = "<img class='rockWin' src='./img/happyStone.jpg'>";
+        else
+        image = "<img class='rockWin' src='./img/sadStone.jpg'>";
     break;
     case 2 :
+        if(bool)    
         image = "<img class='paperWin' src='./img/happyPaper.jpg'>";
+        else
+        image = "<img class='paperWin' src='./img/sadPaper.webp'>";
     break;
-    case 3 :
+    case 3 :   
+        if(bool)    
         image = "<img class='scissorsWin' src='./img/happyScissors.jpg'>";
+        else
+        image = "<img class='scissorsWin' src='./img/sadScissors.jpg'>";
     break;
-}
+    }
 
 let div = document.createElement("div");
 div.innerHTML = image;
 contain.appendChild(div);
-
 }
-res(optUser, img, contenedorUser);
-res(optComp, img2, contenedorComp);
 
  let result = document.getElementById("containerResult");
 
-if(optUser == optComp ){ 
+ if(optUser == optComp ){ 
     result.innerHTML = '<img class="result" src="./img/Dead-Heat.webp" alt="deadHeat"> '; 
 }else if(optUser == 1 && optComp == 3 || optUser == 2 && optComp == 1 
     || optUser == 3 && optComp == 2 ){
+        userBool = true;
         result.innerHTML = '<img class="result" src="./img/win.png" alt="resultWin"> ';
     }else {
+        compBool  = true;
          result.innerHTML = '<img class="result" src="./img/lost2.jpg" alt="resultLost">';
          }
+
+res(optUser,  contenedorUser, userBool);
+res(optComp,  contenedorComp, compBool);
