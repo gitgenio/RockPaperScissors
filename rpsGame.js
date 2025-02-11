b83e0f37e77369093a14630d770f390784347559
ctoro <catorog@unal.edu.co>
2025-02-10 22:11:06 -0500

    modify images

--- rpsGame.js
+++ rpsGame.js
@@ -1,40 +1,45 @@
 
+/* Función que almacena el número seleccionado en localStorage */
 const changeNumber = (number) => {  
    localStorage.keepNum = number; 
 }
 
- const objStone = {
-   img : '<a href="./rpsgamePage2.html"  > <img src="./img/happyStone.jpg"> </a>',
+/* Objetos que representan las opciones del juego (piedra, papel y tijeras) */
+const objStone = {
+   img : '<a href="./rpsgamePage2.html"  > <img src="./img/stone/happyStone.png"> </a>',
    num :1,
- };
- const objPaper = {
-   img : '<a class="paper" href="./rpsgamePage2.html" >  <img  id="idPaper" src="./img/happyPaper.jpg" > </a> ',
+};
+
+const objPaper = {
+   img : '<a class="paper" href="./rpsgamePage2.html" >  <img  id="idPaper" src="./img/paper/happyPaper.png" > </a> ',
    num :2,
- };
- const objScissors = {
-   img : '<a class="scissors" href="./rpsgamePage2.html">   <img  id="idScissors"  src="./img/happyScissors.jpg" > </a> ',
+};
+
+const objScissors = {
+   img : '<a class="scissors" href="./rpsgamePage2.html">   <img  id="idScissors"  src="./img/scissors/happyScissors.png" > </a> ',
    num :3,
- };
+};
 
+/* Selección del contenedor donde se insertarán las opciones del juego */
+let contain = document.querySelector(".images");
 
-   let contain = document.querySelector(".images");
+/* Creación del elemento para la opción 'Piedra' */
+let divStone = document.createElement("div");
+// La función toma el dato cuando se hace click
+divStone.addEventListener("click", () => changeNumber(objStone.num));
+divStone.innerHTML = objStone.img;
+contain.appendChild(divStone);
 
-   let divStone = document.createElement("div");
-   // la función toma el dato cuando le hacen click
-   divStone.addEventListener("click", () => changeNumber(objStone.num));
-   // No sirve,  la función toma el dato de la ultima vez que se ejecuta
-   // divStone.addEventListener("click", changeNumber(objStone.num));   
-   divStone.innerHTML = objStone.img;
-   contain.appendChild(divStone);
+/* Creación del elemento para la opción 'Papel' */
+let divPaper = document.createElement("div");
+divPaper.addEventListener("click", () => changeNumber(objPaper.num));
+divPaper.innerHTML = objPaper.img;
+contain.appendChild(divPaper);
 
-   let divPaper = document.createElement("div");
-   divPaper.addEventListener("click", () => changeNumber(objPaper.num));
-   divPaper.innerHTML = objPaper.img;
-   contain.appendChild(divPaper);
-
-   let divScissors = document.createElement("div");
-   divScissors.addEventListener("click", () => changeNumber(objScissors.num));
-   divScissors.innerHTML = objScissors.img;
-   contain.appendChild(divScissors);
+/* Creación del elemento para la opción 'Tijeras' */
+let divScissors = document.createElement("div");
+divScissors.addEventListener("click", () => changeNumber(objScissors.num));
+divScissors.innerHTML = objScissors.img;
+contain.appendChild(divScissors);
 
    
--- rpsGame2.js
+++ rpsGame2.js
@@ -1,49 +1,55 @@
+/* Recupera la opción seleccionada por el usuario desde localStorage y la convierte en número */
 let optUser = parseInt(localStorage.keepNum);
+
+/* Genera una opción aleatoria para la computadora (1 a 3) */
 const optComp = Math.floor( Math.random() * ( 4 - 1 ) ) + 1; 
+
+/* Selecciona los contenedores donde se mostrarán las elecciones del usuario y la computadora */
 const contenedorUser = document.querySelector(".containUser");
 const contenedorComp = document.querySelector(".containerComp"); 
+
 let userBool = false;
 let compBool = false;
 
-function res(option,  contain, bool){
+/* Función que muestra la imagen correspondiente según la elección y el resultado */
+function res(option, contain, bool){
+    let image;
     switch(option){
-    case 1 :
-        if(bool)
-        image = "<img class='rockWin' src='./img/happyStone.jpg'>";
-        else
-        image = "<img class='rockWin' src='./img/sadStone.jpg'>";
-    break;
-    case 2 :
-        if(bool)    
-        image = "<img class='paperWin' src='./img/happyPaper.jpg'>";
-        else
-        image = "<img class='paperWin' src='./img/sadPaper.webp'>";
-    break;
-    case 3 :   
-        if(bool)    
-        image = "<img class='scissorsWin' src='./img/happyScissors.jpg'>";
-        else
-        image = "<img class='scissorsWin' src='./img/sadScissors.jpg'>";
-    break;
+        case 1:
+            image = bool ? "<img class='rockWin' src='./img/stone/happyStone.png'>" : "<img class='rockWin' src='./img/stone/sadStone.png'>";
+            break;
+        case 2:
+            image = bool ? "<img class='paperWin' src='./img/paper/happyPaper.png'>" : "<img class='paperWin' src='./img/paper/sadPaper.png'>";
+            break;
+        case 3:
+            image = bool ? "<img class='scissorsWin' src='./img/scissors/happyScissors.png'>" : "<img class='scissorsWin' src='./img/scissors/sadScissors.png'>";
+            break;
     }
-
-let div = document.createElement("div");
-div.innerHTML = image;
-contain.appendChild(div);
+    
+    /* Crea un nuevo div y agrega la imagen correspondiente */
+    let div = document.createElement("div");
+    div.innerHTML = image;
+    contain.appendChild(div);
 }
 
- let result = document.getElementById("containerResult");
+/* Selecciona el contenedor donde se mostrará el resultado */
+let result = document.getElementById("containerResult");
 
- if(optUser == optComp ){ 
+/* Lógica para determinar el resultado del juego */
+if(optUser == optComp){ 
     result.innerHTML = '<img class="result" src="./img/Dead-Heat.webp" alt="deadHeat"> '; 
-}else if(optUser == 1 && optComp == 3 || optUser == 2 && optComp == 1 
-    || optUser == 3 && optComp == 2 ){
+} else if(
+    (optUser == 1 && optComp == 3) ||
+    (optUser == 2 && optComp == 1) ||
+    (optUser == 3 && optComp == 2) ){
         userBool = true;
         result.innerHTML = '<img class="result" src="./img/win.png" alt="resultWin"> ';
-    }else {
+} else {
         compBool  = true;
-         result.innerHTML = '<img class="result" src="./img/lost2.jpg" alt="resultLost">';
-         }
+        result.innerHTML = '<img class="result" src="./img/lost2.jpg" alt="resultLost">';
+}
 
-res(optUser,  contenedorUser, userBool);
-res(optComp,  contenedorComp, compBool);
+/* Llamada a la función para mostrar las imágenes correspondientes */
+res(optUser, contenedorUser, userBool);
+res(optComp, contenedorComp, compBool);
+
