9cc679f5fec6fffa526e84c5bc92a52153250363
ctoro <catorog@unal.edu.co>
2025-02-20 01:09:14 -0500

    score and button reboot

--- rpsGame.js
+++ rpsGame.js
@@ -1,3 +1,5 @@
+let contUser = localStorage.keepUserWins;
+let contPc = localStorage.keepPcWins;
 
 /* Función que almacena el número seleccionado en localStorage */
 const changeNumber = (number) => {  
@@ -6,7 +8,7 @@ const changeNumber = (number) => {
 
 /* Objetos que representan las opciones del juego (piedra, papel y tijeras) */
 const objStone = {
-   img : '<a href="./rpsgamePage2.html"  > <img src="./img/stone/happyStone.png"> </a>',
+   img : '<a class="rock" href="./rpsgamePage2.html"  > <img id="idRock" src="./img/stone/happyStone.png"> </a>',
    num :1,
 };
 
@@ -42,4 +44,33 @@ divScissors.addEventListener("click", () => changeNumber(objScissors.num));
 divScissors.innerHTML = objScissors.img;
 contain.appendChild(divScissors);
 
-   
+let containScore = document.querySelector(".score");
+// Crea el título "SCORE"
+let scoreTitle = document.createElement("h2");
+scoreTitle.innerHTML = "SCORE";
+containScore.appendChild(scoreTitle);
+
+// Crear un contenedor flexible para los contadores
+let countersContainer = document.createElement("div");
+// le agrega la clase al div 
+countersContainer.classList.add("counters-container"); 
+
+// Crear el contador del usuario
+let userCounter = document.createElement("p");
+userCounter.innerHTML = "User: "+ contUser;
+countersContainer.appendChild(userCounter);
+
+// Crear el contador de la PC
+let pcCounter = document.createElement("p");
+pcCounter.innerHTML = "PC: "+ contPc;
+countersContainer.appendChild(pcCounter);
+
+// Agregar el contenedor de contadores al contenedor principal
+containScore.appendChild(countersContainer);
+
+let buttonReboot = document.getElementById("button-reboot");
+buttonReboot.addEventListener("click", () => {
+   localStorage.keepUserWins = 0;
+   localStorage.keepPcWins = 0;
+   location.reload();
+});
--- rpsGame2.js
+++ rpsGame2.js
@@ -1,55 +1,87 @@
-/* Recupera la opción seleccionada por el usuario desde localStorage y la convierte en número */
+// Se inicializan userWins y pcWins con los valores almacenados en localStorage, si existen.
+let userWins = localStorage.keepUserWins ? parseInt(localStorage.keepUserWins) : 0;
+let pcWins = localStorage.keepPcWins ? parseInt(localStorage.keepPcWins) : 0;
+// Recupera la opción seleccionada por el usuario desde localStorage y la convierte en número 
 let optUser = parseInt(localStorage.keepNum);
 
+// Elementos HTML donde se mostrará el puntaje
+const userScoreElement = document.getElementById("userScore");
+const pcScoreElement = document.getElementById("pcScore");
+
+// Función para actualizar los puntajes en localStorage y en pantalla
+const updateScores = () => {
+    localStorage.keepUserWins = userWins;
+    localStorage.keepPcWins = pcWins;
+    userScoreElement.textContent = userWins;
+    pcScoreElement.textContent = pcWins;
+  };
+
 /* Genera una opción aleatoria para la computadora (1 a 3) */
-const optComp = Math.floor( Math.random() * ( 4 - 1 ) ) + 1; 
+const optComp = Math.floor(Math.random() * (4 - 1)) + 1;
 
 /* Selecciona los contenedores donde se mostrarán las elecciones del usuario y la computadora */
 const contenedorUser = document.querySelector(".containUser");
-const contenedorComp = document.querySelector(".containerComp"); 
+const contenedorComp = document.querySelector(".containerComp");
 
 let userBool = false;
 let compBool = false;
 
 /* Función que muestra la imagen correspondiente según la elección y el resultado */
-function res(option, contain, bool){
-    let image;
-    switch(option){
-        case 1:
-            image = bool ? "<img class='rockWin' src='./img/stone/happyStone.png'>" : "<img class='rockWin' src='./img/stone/sadStone.png'>";
-            break;
-        case 2:
-            image = bool ? "<img class='paperWin' src='./img/paper/happyPaper.png'>" : "<img class='paperWin' src='./img/paper/sadPaper.png'>";
-            break;
-        case 3:
-            image = bool ? "<img class='scissorsWin' src='./img/scissors/happyScissors.png'>" : "<img class='scissorsWin' src='./img/scissors/sadScissors.png'>";
-            break;
-    }
-    
-    /* Crea un nuevo div y agrega la imagen correspondiente */
-    let div = document.createElement("div");
-    div.innerHTML = image;
-    contain.appendChild(div);
+function res(option, contain, bool) {
+  let image;
+  switch (option) {
+    case 1:
+      image = bool
+        ? "<img class='rockWin' src='./img/stone/happyStone.png'>"
+        : "<img class='rockWin' src='./img/stone/sadStone.png'>";
+      break;
+    case 2:
+      image = bool
+        ? "<img class='paperWin' src='./img/paper/happyPaper.png'>"
+        : "<img class='paperWin' src='./img/paper/sadPaper.png'>";
+      break;
+    case 3:
+      image = bool
+        ? "<img class='scissorsWin' src='./img/scissors/happyScissors.png'>"
+        : "<img class='scissorsWin' src='./img/scissors/sadScissors.png'>";
+      break;
+  }
+
+  /* Crea un nuevo div y agrega la imagen correspondiente */
+  let div = document.createElement("div");
+  div.innerHTML = image;
+  contain.appendChild(div);
 }
 
 /* Selecciona el contenedor donde se mostrará el resultado */
 let result = document.getElementById("containerResult");
 
 /* Lógica para determinar el resultado del juego */
-if(optUser == optComp){ 
-    result.innerHTML = '<img class="result" src="./img/Dead-Heat.webp" alt="deadHeat"> '; 
-} else if(
-    (optUser == 1 && optComp == 3) ||
-    (optUser == 2 && optComp == 1) ||
-    (optUser == 3 && optComp == 2) ){
-        userBool = true;
-        result.innerHTML = '<img class="result" src="./img/win.png" alt="resultWin"> ';
+if (optUser == optComp) {
+  result.innerHTML =
+    '<img class="result" src="./img/Dead-Heat.webp" alt="deadHeat"> ';
+} else if (
+  (optUser == 1 && optComp == 3) ||
+  (optUser == 2 && optComp == 1) ||
+  (optUser == 3 && optComp == 2)
+) {
+  userBool = true;
+  result.innerHTML =
+    '<img class="result" src="./img/win.png" alt="resultWin"> ';  
+  userWins += 1; // Usuario gana una ronda 
 } else {
-        compBool  = true;
-        result.innerHTML = '<img class="result" src="./img/lost2.jpg" alt="resultLost">';
+  compBool = true;
+  result.innerHTML =
+    '<img class="result" src="./img/lost2.jpg" alt="resultLost">';
+  pcWins += 1; // PC gana una ronda 
 }
 
 /* Llamada a la función para mostrar las imágenes correspondientes */
 res(optUser, contenedorUser, userBool);
 res(optComp, contenedorComp, compBool);
+
+// Actualizar puntajes después de cada ronda
+updateScores();
+
+
 
